5a8b933d93e4c1e3de61dbe75d9c4b16a1afa0f3
Corie Beale <bealecorie@gmail.com>
2024-09-12 19:52:34 -0400

    complete

---
+++ src/imageResponses.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const img = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getMeme = (request, response) => {
+    response.writeHead(200, {'Content-Type': 'image/png'}); // according to mdn: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+    response.write(img);
+    response.end();
+}
+
+module.exports = {getMeme};
