3fc9ccd775b197d85e40ca3f75ce1befc2df44a2
Corie Beale <bealecorie@gmail.com>
2024-09-17 18:57:57 -0400

    added rich-client endpoint

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const richClient = fs.readFileSync(`${__dirname}/../client/rich-client.html`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
@@ -15,5 +16,10 @@ const getPage2 = (request, response) => {
   response.end();
 };
 
-module.exports.getIndex = getIndex;
-module.exports.getPage2 = getPage2;
+const getRichClient = (requrest, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(richClient);
+  response.end();
+};
+
+module.exports = { getIndex, getPage2, getRichClient };
